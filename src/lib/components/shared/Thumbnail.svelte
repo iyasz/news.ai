0cc92cf48eb98d63809f5dfe883bcba94e2b9b7f
Muhammad Ilyasa <zakamaragames@gmail.com>
2025-08-22 11:43:06 +0700

    refactor: allocating function at Thumbnail component

--- src/lib/components/shared/Thumbnail.svelte
+++ src/lib/components/shared/Thumbnail.svelte
@@ -2,7 +2,11 @@
     import defaultThumbnail from "$lib/assets/default_thumbnail.png"
     export let src = "";
     export let styleClass = "";
+
+    function handleError(e) {
+        e.currentTarget.src = defaultThumbnail;
+    }
 </script>
 
 
-<img src={src || defaultThumbnail} alt="thumbnail" loading="lazy" on:error={(e) => e.currentTarget.src = defaultThumbnail} class="{styleClass} bg-gray-200 bg-center object-cover shadow-sm "/>
+<img src={src || defaultThumbnail} alt="thumbnail" loading="lazy" on:error={handleError} class="{styleClass} bg-gray-200 bg-center object-cover shadow-sm "/>
